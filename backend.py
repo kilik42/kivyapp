ee9761d0f5e05e25386cbd8722bb8511bf1a3c22
marvin evins <maevins@cps.edu>
2020-02-27 04:53:32 -0600

    finished frontend

---
+++ backend.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def connect():
+
+
+
+def insert(title, author, year, isbn):
+
+
+def view():
+
+
+def search(title="", author="", year="", isbn=""):
+
+def delete(id):
+
+
+
+def update(id, title, author, year, isbn):
+
+
+
+connect()
+
+
+print(view())
